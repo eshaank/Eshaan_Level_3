20832dbb89d40379db04bee6e93346c226385bd7
League <ewbrowne743@gmail.com>
2017-08-22 14:28:44 -0700

    asdf
    asdf

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -37,45 +37,64 @@ public class Algorithms {
 
 	public static double findTallest(List<Double> peeps) {
 		double tall = 0;
-		for(Double o : peeps) {
+		for (Double o : peeps) {
 			peeps.indexOf(o);
-			
-			if(o > tall ) {
+
+			if (o > tall) {
 				tall = o;
-				System.out.println(o);	
-						}
-		
+				System.out.println(o);
+			}
+
 		}
 		return tall;
 
 	}
-	
+
 	public static String findLongestWord(List<String> words) {
-		
+
 		String size1 = "";
-		for(String o : words) {
+		for (String o : words) {
 			if (size1.length() < o.length()) {
 				o.indexOf(o);
 				size1 = o;
-				
-//			words.indexOf(o);
-			System.out.println(o);
-			
+
+				// words.indexOf(o);
+				System.out.println(o);
+
 			}
-		
+
 		}
 		return size1;
-		
+
 	}
-	
-	
 
 	public static Boolean containsSOS(List<String> message1) {
-	
-		if (message1.contains("... --- ...")) {
-			return true;
+		for (String o : message1) {
+			System.out.println(o);
+			if (o.contains("... --- ...")) {
+				System.out.println("hi");
+				return true;
+			}
+
+		}
+		return false;
+	}
+
+	public static List<Double> sortScores(List<Double> results) {
+
+		for (int i = 0; i < results.size(); i++) {
+			for (int j = i + 1; j < results.size(); j++) {
+				if (results.get(j) < results.get(i)) {
+					double temp = results.get(i);
+					results.set(i, results.get(j));
+					results.set(j, temp);
+				}
+
+			}
+
 		}
 
-return null;
+		return results;
+
 	}
 }
